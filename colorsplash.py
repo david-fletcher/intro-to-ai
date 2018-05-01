51c6862dc8916c6408dc0490a8f27e5e165fea2d
david-fletcher <david_fletcher@taylor.edu>
2018-05-01 18:43:29 -0400

    Initialized a new folder for our opencv project
    these are just the source files and sample images I'm using. You'll need to install opencv yourself

---
+++ colorsplash.py
@@ -0,0 +1,7 @@
+import cv2 as cv
+import numpy as np
+
+img = cv.imread('greenpaper.JPG')
+
+
+#TODO: read in filename, and (row, col) coordinates from the command line -- like maybe: 'python colorsplash.py greenpaper.JPG 1345 672' and incorporate those values into making the colorsplash
